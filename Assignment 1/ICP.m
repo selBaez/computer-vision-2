70b2712486e34068a3be8d5182097e95ade190a7
selBaez <sel.bs.93@hotmail.com>
2016-04-16 01:01:49 +0200

    Phase 1 and 2

--- Assignment 1/ICP.m
+++ Assignment 1/ICP.m
@@ -11,13 +11,27 @@ else
     T = zeros(1,3);
     
     %% Phase 1: Find closest points
+    num_points = 100;
     
-    % n2 = dist2(pcd_base, pcd_target); %currently throwing an "out of
-    % memory" error
+    %n2 = zeros(num_points, num_points);
+    n2 = dist2(pcd_base(1:num_points, :), pcd_target(1:num_points, :)); % currently throwing an "out of
+    % memory" error when using whole point cloud
+    
+    % For each row in n2 (each point in base), get column with lowest value
+    % (closest point in target)
+    [dist idx] = min(n2, [], 2);
+    
+    pcd_matched = pcd_target(idx,:);
 
     %% Phase 2: finding the geometric centroid 
     % Bc: center of base cloud
+    Bc = mean(pcd_base(1:num_points, :));
     % Tc: center of target cloud 
+    Tc = mean(pcd_matched);
+    
+    % Subtract centroid %TODO Revise this part
+    new_pcd_base   = pcd_base - Bc;
+    new_pcd_target = pcd_target - Tc;
     
     %% Phase 3: Apply singular value decomposition
     % FOR ALL matched point pair AS i:
