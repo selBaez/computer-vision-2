b5eae1a51e6f469a9d881766094119fab3c1832b
selBaez <sel.bs.93@hotmail.com>
2016-04-19 17:29:19 +0200

    Update test images

--- Assignment 1/test.m
+++ Assignment 1/test.m
@@ -4,6 +4,6 @@ load('target.mat');
 pcd_b = source';
 pcd_t = target';
 
-[num_points, dimensions] = size(pcd_b);
+num_points = min(size(pcd_b, 1), size(pcd_t, 1));
 
 [pcd_b, new_pcd_t, R, T] = own_ICP(pcd_b, pcd_t, num_points);
