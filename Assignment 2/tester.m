f67cd20a27e777e98ede21b3e2e849b0e51af16e
selBaez <sel.bs.93@hotmail.com>
2016-05-04 00:13:13 +0200

    Run funMat

--- Assignment 2/tester.m
+++ Assignment 2/tester.m
@@ -1,6 +1,6 @@
-% run('vlfeat-0.9.20/toolbox/vl_setup.m');
-im1_path = '/House/frame00000001.png';
-im2_path = '/House/frame00000002.png';
+run('vlfeat-0.9.20/toolbox/vl_setup.m');
+im1_path = 'House/frame00000001.png';
+im2_path = 'House/frame00000002.png';
 
 im1 = imread(im1_path);
 im2 = imread(im2_path);
