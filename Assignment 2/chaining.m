0c66087fce9393a483a706f5a4529eb0802523f3
Ildefonso Pica <ildefonsofpica@gmail.com>
2016-05-10 18:42:31 +0200

    Duplicate removal + bug fixes: column index calculation.

--- Assignment 2/chaining.m
+++ Assignment 2/chaining.m
@@ -3,32 +3,38 @@
 % The columns of the matrix represent points
 
 im_format = 'House/frame000000%02d.png';
-nfirst = 0;
+nfirst = 1;
 nim = 49;
 
 im_indexes = nfirst+1:nim;
 
 for im = im_indexes
+    fprintf('Image: %i of %i\n', im, nim)
+    
     impath1 = sprintf(im_format, im - 1);
     impath2 = sprintf(im_format, im);  
     
-    im1 = imread(im1_path);
-    im2 = imread(im2_path);
+    im1 = imread(impath1);
+    im2 = imread(impath2);
 
-    if im == 1
+    if im == 2
         % INITIAL STEP
         pvMat = funMat(im1, im2, 2, 0);
+        % Remove duplicate points
+        pvMat = unique(pvMat','rows')';
     else
-        fprintf('HI')
-        pvMat = vertcat(zeros(2,length(pvMat)));
+        last_pv = pvMat(end-1:end,:)';
+        pvMat = vertcat(pvMat, zeros(2,length(pvMat)));
+        
         [row, col] = size(pvMat);
         points = funMat(im1, im2, 2, 0);
-        last_pv = pvMat(end-1:end)';
-        this_pv = points(1:2)';
+        this_pv = points(1:2,:)';
         
         for n = 1:length(points)
             % Find column index
-            idx = find(last_pv(ismember(last_pv,this_pv(n,:),'rows'),1));
+            idx = find(ismember(last_pv,this_pv(n,:),'rows'),1);
+%             fprintf('Match index: %i, point: %i of %i\n', idx, n, length(points))
+            
             if ~isempty(idx)
                 % Add the new view, to the corresponding point-column
                 pvMat(row-1:row, idx) = points(3:4,n);
