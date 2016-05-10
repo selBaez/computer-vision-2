d11eb92bdc26f4ed6eb960967d86b552e172d2d8
selBaez <sel.bs.93@hotmail.com>
2016-05-10 17:06:05 +0200

    iterate over images

--- Assignment 2/chaining.m
+++ Assignment 2/chaining.m
@@ -1,14 +1,28 @@
 % Construct a point-view matrix
 % The rows of the matrix represent images
 % The columns of the matrix represent points
-for im = 1:40-1
+
+im_format = 'House/frame000000%02d.png';
+nfirst = 0;
+nim = 49;
+
+im_indexes = nfirst+1:nim;
+
+for im = im_indexes
+    impath1 = sprintf(im_format, im - 1);
+    impath2 = sprintf(im_format, im);  
+    
+    im1 = imread(im1_path);
+    im2 = imread(im2_path);
+
     if im == 1
         % INITIAL STEP
-        pvMat = funMat(im, im+1, 2, 0);
+        pvMat = funMat(im1, im2, 2, 0);
     else
+        fprintf('HI')
         pvMat = vertcat(zeros(2,length(pvMat)));
         [row, col] = size(pvMat);
-        points = funMat(im, im+1, 2, 0);
+        points = funMat(im1, im2, 2, 0);
         last_pv = pvMat(end-1:end)';
         this_pv = points(1:2)';
         
